cd6c6ea2ecf4d5b8e5cdfb63f540bc13d25001e8
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-04 23:55:21 +0200

    refactor the code and add deposit unit test

--- test/Bank.js
+++ test/Bank.js
@@ -1,60 +1,47 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const BigNumber = require("bignumber.js");
 
 describe("Bank", function () {
-  async function deploy() {
+  let bank, bank_owner;
+
+  before(async function () {
     const [owner, otherAccount] = await ethers.getSigners();
-
     const Bank = await ethers.getContractFactory("Bank");
-    const bank = await Bank.deploy(owner.address);
-
-    return { bank, owner, otherAccount };
-  }
-
-  it("Should set the contract owner", async function () {
-    const { bank, owner } = await deploy();
-    const contractOwner = await bank.getContractOwner();
-    expect(owner.address).to.be.equal(contractOwner);
+    bank = await Bank.deploy(owner.address);
+    bank_owner = owner.address;
   });
 
-  it("Should create an account", async function () {
-    const { bank, owner } = await deploy();
-    const lockTimeBigNumber = new Date(2023, 2, 27, 13, 30, 0).getTime();
+  it("Should set the contract owner", async function () {
+    const contractOwner = await bank.getContractOwner();
+    expect(bank_owner).to.be.equal(contractOwner);
+  });
 
+  it("Should create & get the account", async function () {
     const account = {
-      owner: owner.address,
       accountName: "Celo Bank",
       description: "Safe and decentralized bank",
-      amount: 0,
-      lockTime: lockTimeBigNumber,
       accountId: 2390,
     };
-    /* 
-     create account requires
-     params: accountName, accountDescription, AccountId
-     returns true
-    */
     await bank.createAccount(
       account.accountName,
       account.description,
-      account.lockTime,
       account.accountId
     );
-
-    /* getAccount requires the accountId */
     const accountCreated = await bank.getAccount(2390);
 
-    expect(accountCreated.owner).to.equal(account.owner);
+    expect(accountCreated.owner).to.equal(bank_owner);
     expect(accountCreated.name).to.equal(account.name);
     expect(accountCreated.description).to.equal(account.description);
-    expect(accountCreated.amount).to.equal(account.amount);
-    expect(accountCreated.id).to.equal(account.id);
-    expect(accountCreated.lockTime).to.equal(account.lockTime);
+    expect(accountCreated.amount).to.equal(0);
+    expect(accountCreated.accountId).to.equal(account.accountId);
+    expect(accountCreated.lockTime).to.equal(0);
+  });
+
+  it("Should make a deposit", async function () {
+    const depositAmount = ethers.utils.parseEther("1");
+    const depositReceipt = await bank.deposit(depositAmount, 2390);
+
+    const txReceipt = await depositReceipt.wait();
+    expect(depositAmount).to.equal(txReceipt.events[0].args[0]);
   });
 });
