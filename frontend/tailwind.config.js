cc53869eeb273fbc5b4311087d2bf2e72a792db5
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-07 20:36:36 +0200

    setup the frontapp

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
